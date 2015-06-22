1973035324f1f328be209c8661d192f879240ec8
Matt Liebetrau <matt.liebetrau@animoto.com>
2015-06-22 14:41:50 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Munchie::Application.config.secret_token = 'e5b2cd69d54d4fc845f3ac380aa035e2e82499478a29ba469a6291d13ea6d7d81686701a0046f689452a590c83c7b6805bc0f6c0631fd38a2618c274de60178a'
