01850062a90430bcc0f441e431ec1f50d442ede0
Justin Camerer <iamjwc@gmail.com>
2015-06-23 11:54:00 -0500

    Munch interactor

--- app/models/munch_interactor.rb
+++ app/models/munch_interactor.rb
@@ -1,11 +1,23 @@
 class MunchInteractor
   def self.munch(params)
     user = user_from_params(params)
+    command = parse_command(params[:text])
 
-    "Hey @#{user.slack_handle} `#{params[:text]}` yourself!"
+    "I got @#{user.slack_handle} type: `#{command[:type]}` args: `#{command[:args]}`"
   end
 
-  def user_from_params(params)
+  def self.parse_command(text)
+    text = text.to_s.strip
+
+    if text =~ /([^\s]*)\s*(.*)/
+      {
+        :type => $1,
+        :args => $2.strip,
+      }
+    end
+  end
+
+  def self.user_from_params(params)
     User.where({
       :slack_handle => params[:user_name].downcase
     }).first_or_create
